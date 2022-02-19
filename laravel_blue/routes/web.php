d437e203b7873243919ed073afcdac787ad78631
yu405405 <66393193+you405405@users.noreply.github.com>
2022-02-19 18:45:35 +0900

    コントローラの利用

--- laravel_blue/routes/web.php
+++ laravel_blue/routes/web.php
@@ -11,27 +11,9 @@
 |
 */
 
+use App\Http\Controllers\HelloController;
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-Route::get('hello/{msg?}', function($msg="no message.") {
-    $html = <<< EOF
-    <html>
-    <head>
-        <style>
-            body {font-size: 16pt; color: #999;}
-            h1 {font-size: 100pt; text-align: right; color: #eee margin: -40px 0px -50px 0px;}
-        </style>
-    </head>
-    <body>
-        <h1>Good morning</h1>
-        <p>{$msg}</p>
-        <p>これは、サンプルで作ったページです。</p>
-    </body>
-    </html>
-    EOF;
-    return $html;
-});
+Route::get('hello', 'HelloController@index');
