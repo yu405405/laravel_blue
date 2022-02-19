ca8e49f75dc3a55d3cf3ddefc3c8fcd8f6beae53
yu405405 <66393193+you405405@users.noreply.github.com>
2022-02-19 16:14:01 +0900

    2-1.ルーティング

--- laravel_blue/routes/web.php
+++ laravel_blue/routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('hello/{msg?}', function($msg="no message.") {
+    $html = <<< EOF
+    <html>
+    <head>
+        <style>
+            body {font-size: 16pt; color: #999;}
+            h1 {font-size: 100pt; text-align: right; color: #eee margin: -40px 0px -50px 0px;}
+        </style>
+    </head>
+    <body>
+        <h1>Good morning</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+    return $html;
+});
