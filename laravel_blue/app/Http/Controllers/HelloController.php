af93a04d2913755a6e8c0b6569ac653f931b980a
yu405405 <66393193+you405405@users.noreply.github.com>
2022-02-21 08:36:10 +0900

    サービスとビューコンポーザ

--- laravel_blue/app/Http/Controllers/HelloController.php
+++ laravel_blue/app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            ['name' => '山田たろう', 'mail' => 'taro@yamada'],
-            ['name' => '田中はなこ', 'mail' => 'hanako@flower'],
-            ['name' => '鈴木さちこ', 'mail' => 'sachico@happy']
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index', ['message'=>'hello!']);
     }
 
     public function post(Request $request) {
--- laravel_blue/resources/views/hello/index.blade.php
+++ laravel_blue/resources/views/hello/index.blade.php
@@ -9,24 +9,14 @@
 
 @section('content')
     <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述できます。</p>
-
-    <ul>
-        @each('components.item', $data, 'item')
-    </ul>
-    <!-- @include('components.message', [
-            'msg_title' => 'OK',
-            'msg_content' => 'サブビューだよ.'
-        ]) -->
-    <!-- @component('components.message')
-        @slot('msg_title')
-            CAUTION!
-        @endslot
-
-        @slot('msg_content')
-            これはメッセージの表示です。
-        @endslot
-    @endcomponent -->
+    <p>
+        Controller value<br>
+        'message' = {{$message}}
+    </p>
+    <p>
+        ViewComposer value<br>
+        'view_message' = {{$view_message}}
+    </p>
 @endsection
 
 @section('footer')
