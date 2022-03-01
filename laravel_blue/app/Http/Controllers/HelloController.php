b903746a11c90de04e9b8b74959a0d5b39cdb5d9
yu405405 <66393193+you405405@users.noreply.github.com>
2022-03-01 21:21:47 +0900

    クエリビルダ

--- laravel_blue/app/Http/Controllers/HelloController.php
+++ laravel_blue/app/Http/Controllers/HelloController.php
@@ -13,21 +13,20 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('hello.index', ['items' => $items]);
     }
-
     public function post(Request $request)
     {
         $items = DB::select('select * from people');
         return view('hello.index', ['item' => $items]);
     }
 
+
     public function add(Request $request)
     {
         return view('hello.add');
     }
-
     public function create(Request $request)
     {
         $param = [
@@ -35,48 +34,53 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values(:name, :mail, :age)', $param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
     }
-
 
 
     public function edit(Request $request)
     {
         $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
     }
-
     public function update(Request $request)
     {
         $param = [
-            'id' => $request->id,
             'name' => $request->name,
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
         return redirect('/hello');
     }
 
 
     public function del(Request $request)
     {
-        $param = [
-            'id' => $request->id
-        ];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
     }
-    
     public function remove(Request $request)
     {
-        $param = [
-            'id' => $request->id
-        ];
-        DB::delete('delete from people where id = :id', $param);
+        DB::table('people')
+            ->where('id', $request->id)->delete();
         return redirect('/hello');
+    }
+
+    
+    public function show(Request $request)
+    {
+        $page = $request->page;
+        $items = DB::table('people')
+            ->offset($page * 3)
+            ->limit(3)
+            ->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
--- laravel_blue/routes/web.php
+++ laravel_blue/routes/web.php
@@ -31,3 +31,5 @@ Route::post('hello/edit', 'HelloController@update');
 
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+
+Route::get('hello/show', 'HelloController@show');
