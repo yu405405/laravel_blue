b3f587ab450cccac33ce7ae29200adabbcd16d7d
yu405405 <66393193+you405405@users.noreply.github.com>
2022-03-08 08:25:18 +0900

    検索とスコープ

--- laravel_blue/app/Http/Controllers/PersonController.php
+++ laravel_blue/app/Http/Controllers/PersonController.php
@@ -20,7 +20,9 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
         $param = [
             'input' => $request->input,
             'item' => $item
--- laravel_blue/resources/views/person/find.blade.php
+++ laravel_blue/resources/views/person/find.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.helloapp')
 
+@section('title', 'Person.find')
+
 @section('menubar')
     @parent
     検索ページ
